e0a22460f618bcf9aad346a460b6c4a57549f283
Formation <Formation@10.18.0.76>
2017-11-09 12:27:01 +0100

    - version 2.0

--- test/JPATest.java
+++ test/JPATest.java
@@ -21,11 +21,41 @@ public class JPATest {
         EntityManager myEm = myPersistence.createEntityManager();
         myEm.getTransaction().begin();
         
-        Film film = new Film("mon titre1 ","Réalisateur 1" , 1978);
-        myEm.persist(film);
+        Film film1 = new Film("mon titre1 ","Réalisateur 1" , 1911);
+        Film film2 = new Film("mon titre2 ","Réalisateur 2" , 1922);
+        Film film3 = new Film("mon titre3 ","Réalisateur 3" , 1933);
+        Film film4 = new Film("mon titre4 ","Réalisateur 4" , 1944);
+        Film film5 = new Film("mon titre5 ","Réalisateur 5" , 1955);
         
+        myEm.persist(film1);
+        myEm.persist(film2);
+        myEm.persist(film3);
+        myEm.persist(film4);
+        myEm.persist(film5);
+        
+        //film2.setId(film3.getId());
+        //myEm.merge(film2);
+        Film filmToSuppress = myEm.find(Film.class, 1L);
+        System.err.println("Titre1 = "+filmToSuppress.getTitre());
+       //myEm.remove(filmToSuppress);
+       //String qlString="UPDATE APP.MESFILMS SET NOM_TITRE='Titre modif' where ID=1";
+       myEm.createQuery("UPDATE APP.MESFILMS SET NOM_TITRE='Titre modif' where ID=1").executeUpdate();
+       
+        myEm.getTransaction().commit();
                 
         
     }
     
 }
+
+
+
+//TRUNCATE TABLE APP.MESFILMS;
+//INSERT INTO APP.MESFILMS (ID,ANNEE_DE_SORTIE, NOM_REALISATEUR, NOM_TITRE)
+// VALUES
+// (1,1234, 'Armand', 'Saint-Didier-des-Bois'),
+// (2,4567, 'Hebert', 'Marigny-le-Châtel'),
+// (3,7890, 'Ribeiro', 'Maillères'),
+// (4,2144, 'Savary', 'Conie-Molitard');
+//UPDATE APP.MESFILMS SET NOM_TITRE='Titre modif' where ID=1;
+//SELECT * FROM APP.MESFILMS FETCH FIRST 100 ROWS ONLY;
